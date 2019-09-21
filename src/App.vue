a025df3f4e7e65536bfd7aa992139109c567d7dd
jarbun <jarbun306@gmail.com>
2019-09-21 23:03:17 +0530

    Refactor weather card into separate component

--- src/App.vue
+++ src/App.vue
@@ -9,72 +9,26 @@
     <div class="app-loading" v-if="loading">
       <div class="loader"></div>
     </div>
-    <div class="app-content" v-if="location && !loading">
-      <section class="info">
-        <div class="top">
-          <div class="location">
-            <i class="fas fa-map-marker-alt"></i>
-            <span>{{ location }}</span>
-          </div>
-          <div class="buttons">
-            <button @click="toggleUnit" :class="{active: isCelsius}">C</button>
-            <button @click="toggleUnit" :class="{active: !isCelsius}">F</button>
-          </div>
-        </div>
-        <p class="temperature">
-          {{ today.temperature }}
-        </p>
-        <div class="extras">
-          <div>
-            <p>{{ today.feelsLike }}</p>
-            <p class="attribute">Feels Like</p>
-          </div>
-          <div>
-            <p>{{ today.precipChance }}%</p>
-            <p class="attribute">Chance of Rain</p>
-          </div>
-          <div>
-            <p>{{ today.uvIndex }}</p>
-            <p class="attribute">UV Index</p>
-          </div>
-        </div>
-      </section>
-      <section>
-        <p class="summary">{{ today.summary }}</p>
-        <p class="relative-time">Updated {{ timeSince }}</p>
-        <div class="forecasts">
-          <div class="card" v-for="(item, index) in forecasts" :key="index" :class="{active: index === 0}" >
-            <p>{{ item.day }}</p>
-            <i class="fas" :class="item.icon"></i>
-            <span class="range">
-              <p>{{ item.min }}</p>
-              <p>{{ item.max }}</p>
-            </span>
-          </div>
-        </div>
-      </section>
-      <a href="https://darksky.net/poweredby/" class="footer">
-        Powered by DarkSky
-      </a>
-    </div>
+    <WeatherCard :latlong="latlong" :timeSince="timeSince" :weatherData="weatherData" v-else-if="latlong" />
   </main>
 </template>
 
 <script>
+import WeatherCard from './components/WeatherCard';
 
 export default {
   name: 'app',
+  components: {
+    WeatherCard
+  },
   data() {
     return {
+      latlong: null,
+      loading: false,
+      inputValue: '',
+      weatherData: null,
       start: 0,
       timeSince: 0,
-      today: null,
-      forecasts: [],
-      isCelsius: true,
-      location: null,
-      latlong: null,
-      inputValue: '',
-      loading: false
     }
   },
   methods: {
@@ -101,22 +55,9 @@ export default {
         .catch(err => {
           console.log(err);
           this.loading = false;
-        })
+        });
       }
       this.inputValue = '';
-    },
-    toggleUnit() {
-      this.isCelsius = !this.isCelsius;
-    },
-    updateTime() {
-      const diff = (new Date().getTime() - this.start) / 1000;
-
-      if (diff < 60) 
-        this.timeSince = 'just now';
-      else if (diff < 3600) 
-        this.timeSince = `${parseInt(diff/60)} min ago`;
-      else 
-        this.timeSince = `${parseInt(diff/3600)} hr ago`;
     },
     fetchInfo() {
       const key = '85e8f9c3f0ba466ca2512a8008d0df8e';
@@ -125,47 +66,25 @@ export default {
       const units = 'si';
       const url = `${proxy}https://api.darksky.net/forecast/${key}/${this.latlong}?units=${units}&exclude=${exclude}`;
 
-      const icons = {
-        'clear-day': 'fa-sun',
-        'clear-night': 'fa-moon',
-        'rain': 'fa-cloud-rain',
-        'snow': 'fa-snowflake',
-        'sleet': 'fa-cloud-rain',
-        'wind': 'fa-wind',
-        'fog': 'fa-smog',
-        'cloudy': 'fa-cloud',
-        'partly-cloudy-day': 'fa-cloud-sun',
-        'partly-cloudy-night': 'fa-cloud-moon'
-      };
-
       fetch(url)
       .then(res => res.json())
       .then(data => {
-        this.today = {
-          temperature: Math.round(data.currently.temperature),
-          feelsLike: Math.round(data.currently.apparentTemperature),
-          precipChance: Math.round(data.currently.precipProbability),
-          uvIndex: data.currently.uvIndex,
-          summary: data.daily.data[0].summary
-        }
-
-        this.forecasts = [];
-        for (let i = 0; i < 4; i++) {
-          const current = data.daily.data[i];      
-          this.forecasts.push({
-            day: new Date(current.time * 1000).toLocaleDateString('en-US', { weekday: 'short'} ),
-            min: Math.round(current.temperatureMin),
-            max: Math.round(current.temperatureMax),
-            icon: icons[current.icon]
-          });
-        }
-
-        this.location = data.timezone;
+        this.weatherData = data;
 
         this.start = new Date().getTime();
         this.updateTime();
         this.loading = false;
       });
+    },
+    updateTime() {
+      const diff = (new Date().getTime() - this.start) / 1000;
+      console.log('called')
+      if (diff < 60) 
+        this.timeSince = 'just now';
+      else if (diff < 3600) 
+        this.timeSince = `${parseInt(diff/60)} min ago`;
+      else 
+        this.timeSince = `${parseInt(diff/3600)} hr ago`;
     }
   },
   mounted() {
@@ -207,7 +126,14 @@ button {
   cursor: pointer;
 }
 
-input {
+.inputbar {
+  margin-top: 80px;
+  margin-bottom: 16px;
+  display: flex;
+  justify-content: center;
+}
+
+.inputbar input {
   width: 550px;
   height: 50px;
   border: none;
@@ -216,13 +142,6 @@ input {
   font: inherit;
   padding-left: 24px;
   color: var(--black);
-}
-
-.inputbar {
-  margin-top: 80px;
-  margin-bottom: 16px;
-  display: flex;
-  justify-content: center;
 }
 
 .inputbar button {
@@ -235,112 +154,6 @@ input {
 
 .inputbar i {
   font-size: 18px;
-}
-
-.app-content {
-  width: 650px;
-  height: 530px;
-  margin: 0 auto;
-  border-radius: 4px;
-  background-color: white;
-}
-
-.info {
-  height: 240px;
-  border-radius: 4px 4px 0 0;
-  background-color: var(--blue-dark);
-  padding-top: 12px;
-}
-
-.top {
-  padding: 0 12px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.location i {
-  padding: 0 8px;
-  font-size: 18px;
-  color: var(--blue-light);
-}
-
-.buttons button {
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  margin-right: 4px;
-  color: white;
-  background-color: transparent;
-}
-
-.buttons button.active {
-  background-color: var(--blue-light);
-}
-
-.temperature {
-  font-size: 116px;
-}
-
-.extras {
-  display: flex;
-  justify-content: space-around;
-}
-
-.attribute {
-  color: var(--blue-lightest);
-}
-
-.summary {
-  padding-top: 12px;
-  color: var(--black);
-}
-
-.relative-time {
-  font-size: 15px;
-  color: var(--gray);
-}
-
-.forecasts {
-  display: flex;
-  justify-content: space-evenly;
-  padding-top: 20px;
-}
-
-.card {
-  width: 140px;
-  height: 180px;
-  padding-top: 16px;
-  border-radius: 4px;
-  color: var(--blue);
-  border: 1px solid var(--blue-lightest);
-}
-
-.card.active {
-  color: white;
-  background-color: var(--blue-dark);
-}
-
-.card i {
-  display: block;
-  font-size: 56px;
-  text-align: center;
-  padding-top: 24px;
-}
-
-.range {
-  padding-top: 26px;
-  display: flex;
-  justify-content: space-evenly;
-}
-
-.footer {
-  display: block;
-  font-size: 15px;
-  text-align: center;
-  padding: 12px 0;
-  color: var(--gray);
-  text-decoration: none;
 }
 
 .app-loading {
---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,218 @@
+<template>
+  <div v-if="location" class="app-content">
+    <section class="info">
+      <div class="top">
+        <div class="location">
+          <i class="fas fa-map-marker-alt"></i>
+          <span>{{ location }}</span>
+        </div>
+        <div class="buttons">
+          <button @click="toggleUnit" :class="{active: isCelsius}">C</button>
+          <button @click="toggleUnit" :class="{active: !isCelsius}">F</button>
+        </div>
+      </div>
+      <p class="temperature">
+        {{ today.temperature }}
+      </p>
+      <div class="extras">
+        <div>
+          <p>{{ today.feelsLike }}</p>
+          <p class="attribute">Feels Like</p>
+        </div>
+        <div>
+          <p>{{ today.precipChance }}%</p>
+          <p class="attribute">Chance of Rain</p>
+        </div>
+        <div>
+          <p>{{ today.uvIndex }}</p>
+          <p class="attribute">UV Index</p>
+        </div>
+      </div>
+    </section>
+    <section>
+      <p class="summary">{{ today.summary }}</p>
+      <p class="relative-time">Updated {{ timeSince }}</p>
+      <div class="forecasts">
+        <div class="card" v-for="(item, index) in forecasts" :key="index" :class="{active: index === 0}" >
+          <p>{{ item.day }}</p>
+          <i class="fas" :class="item.icon"></i>
+          <span class="range">
+            <p>{{ item.min }}</p>
+            <p>{{ item.max }}</p>
+          </span>
+        </div>
+      </div>
+    </section>
+    <a href="https://darksky.net/poweredby/" class="footer">
+      Powered by DarkSky
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weather-card',
+  props: ['latlong', 'timeSince', 'weatherData'],
+  data() {
+    return {
+      isCelsius: true,
+      today: null,
+      location: null,
+      forecasts: []
+    }
+  },
+  created() {
+    this.parseData();
+  },
+  methods: {
+    toggleUnit() {
+      this.isCelsius = !this.isCelsius;
+    },
+    parseData() {
+      const data = this.weatherData;
+
+      const icons = {
+        'clear-day': 'fa-sun',
+        'clear-night': 'fa-moon',
+        'rain': 'fa-cloud-rain',
+        'snow': 'fa-snowflake',
+        'sleet': 'fa-cloud-rain',
+        'wind': 'fa-wind',
+        'fog': 'fa-smog',
+        'cloudy': 'fa-cloud',
+        'partly-cloudy-day': 'fa-cloud-sun',
+        'partly-cloudy-night': 'fa-cloud-moon'
+      };
+
+      this.today = {
+        temperature: Math.round(data.currently.temperature),
+        feelsLike: Math.round(data.currently.apparentTemperature),
+        precipChance: Math.round(data.currently.precipProbability),
+        uvIndex: data.currently.uvIndex,
+        summary: data.daily.data[0].summary
+      }
+
+      this.forecasts = [];
+      for (let i = 0; i < 4; i++) {
+        const current = data.daily.data[i];      
+        this.forecasts.push({
+          day: new Date(current.time * 1000).toLocaleDateString('en-US', { weekday: 'short'} ),
+          min: Math.round(current.temperatureMin),
+          max: Math.round(current.temperatureMax),
+          icon: icons[current.icon]
+        });
+      }
+
+      this.location = data.timezone;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app-content {
+  width: 650px;
+  height: 530px;
+  margin: 0 auto;
+  border-radius: 4px;
+  background-color: white;
+}
+
+.info {
+  height: 240px;
+  border-radius: 4px 4px 0 0;
+  background-color: var(--blue-dark);
+  padding-top: 12px;
+}
+
+.top {
+  padding: 0 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.location i {
+  padding: 0 8px;
+  font-size: 18px;
+  color: var(--blue-light);
+}
+
+.buttons button {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 4px;
+  color: white;
+  background-color: transparent;
+}
+
+.buttons button.active {
+  background-color: var(--blue-light);
+}
+
+.temperature {
+  font-size: 116px;
+}
+
+.extras {
+  display: flex;
+  justify-content: space-around;
+}
+
+.attribute {
+  color: var(--blue-lightest);
+}
+
+.summary {
+  padding-top: 12px;
+  color: var(--black);
+}
+
+.relative-time {
+  font-size: 15px;
+  color: var(--gray);
+}
+
+.forecasts {
+  display: flex;
+  justify-content: space-evenly;
+  padding-top: 20px;
+}
+
+.card {
+  width: 140px;
+  height: 180px;
+  padding-top: 16px;
+  border-radius: 4px;
+  color: var(--blue);
+  border: 1px solid var(--blue-lightest);
+}
+
+.card.active {
+  color: white;
+  background-color: var(--blue-dark);
+}
+
+.card i {
+  display: block;
+  font-size: 56px;
+  text-align: center;
+  padding-top: 24px;
+}
+
+.range {
+  padding-top: 26px;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.footer {
+  display: block;
+  font-size: 15px;
+  text-align: center;
+  padding: 12px 0;
+  color: var(--gray);
+  text-decoration: none;
+}
+</style>
